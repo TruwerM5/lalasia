1904ecd4d847600d1383e880887fb6df697b7b12
TruwerM5 <ilsur2000.2016@gmail.com>
2022-09-15 21:01:34 +0300

    add changes

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -6,33 +6,60 @@ const sliderEllipses = document.querySelectorAll('[data-slider-ellipse]');
 
 let currentIndex = 0;
 let direction = 'left';
-let translate = 0;
+let translate = 1;
 
 for(let i = 0; i < sliderEllipses.length; i++) {
     sliderEllipses[i].setAttribute('index', i);
 }
 
-function move() {
+function move(start) {
+    let j;
+    if(start != undefined || start != null) {
+        currentIndex = start;
+        j = start;
+        translate = start;
+
+        if(start >= sliderItems.length - 1) {
+            direction = 'right';
+        } else {
+            direction = 'left';
+        }
+    } else j = currentIndex;
+
     if(currentIndex < sliderItems.length - 1 && direction == 'left') {
-        slider__inner.style.transform = `translateX(${translate - 100}%)`;
-        translate -= 100;
+        slider__inner.style.transform = `translateX(${-100*translate}%)`;
+        translate++;
         currentIndex++;
-        if(currentIndex == sliderItems.length - 1) { direction = 'right'; };
+        if(currentIndex == sliderItems.length - 1) { 
+            direction = 'right';
+            translate = 2;
+        };
     } else if(currentIndex > 0 && direction == 'right') {
-        slider__inner.style.transform = `translateX(${translate + 100}%)`;
-        translate += 100;
+        slider__inner.style.transform = `translateX(${-translate * 100}%)`;
+        translate--;
         currentIndex--;
-        if(currentIndex == 0) { direction = 'left'; }
+        if(currentIndex == 0) { 
+            direction = 'left';
+            translate = 1;
+        }
     }
 
     for(let i = 0; i < sliderEllipses.length; i++) {
+       
         sliderEllipses[i].classList.remove('active');
-        if(+sliderEllipses[i].getAttribute('index') == currentIndex) {
+        if(+sliderEllipses[i].getAttribute('index') == j) {
             sliderEllipses[i].classList.add('active');
         }
     }
 }
 
-let timer = setInterval(() => {
-    move();
-}, 5500);
+let timer = setInterval(move, 5500);
+
+
+
+sliderEllipses.forEach((item, index) => {
+    item.addEventListener('click', function() {
+        clearInterval(timer);
+        move(index);
+    });
+})
