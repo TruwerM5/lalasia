eb7e81dba4897b681824bb099dc50a1b223f3c0a
TruwerM5 <ilsur2000.2016@gmail.com>
2022-09-19 21:20:19 +0300

    add changes

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -4,62 +4,46 @@ const slider__inner = document.querySelector('[data-slider]');
 const sliderItems = document.querySelectorAll('[data-slider-item]')
 const sliderEllipses = document.querySelectorAll('[data-slider-ellipse]');
 
-let currentIndex = 0;
+let currentIndex = 1;
 let direction = 'left';
-let translate = 1;
 
 for(let i = 0; i < sliderEllipses.length; i++) {
     sliderEllipses[i].setAttribute('index', i);
 }
 
-function move(start) {
-    let j;
-    if(start != undefined || start != null) {
-        currentIndex = start;
-        j = start;
-        translate = start;
-
-        if(start >= sliderItems.length - 1) {
-            direction = 'right';
-        } else {
-            direction = 'left';
-        }
-    } else j = currentIndex;
-
-    if(currentIndex < sliderItems.length - 1 && direction == 'left') {
-        slider__inner.style.transform = `translateX(${-100*translate}%)`;
-        translate++;
-        currentIndex++;
-        if(currentIndex == sliderItems.length - 1) { 
-            direction = 'right';
-            translate = 2;
-        };
-    } else if(currentIndex > 0 && direction == 'right') {
-        slider__inner.style.transform = `translateX(${-translate * 100}%)`;
-        translate--;
-        currentIndex--;
-        if(currentIndex == 0) { 
-            direction = 'left';
-            translate = 1;
-        }
-    }
-
+function styleButtons(index) {
     for(let i = 0; i < sliderEllipses.length; i++) {
-       
         sliderEllipses[i].classList.remove('active');
-        if(+sliderEllipses[i].getAttribute('index') == j) {
+        if(i == index) {
             sliderEllipses[i].classList.add('active');
         }
     }
 }
 
+function move() {
+    styleButtons(currentIndex);
+    slider__inner.style.transform = `translateX(${-currentIndex * 100}%)`;
+    if(direction == 'left') { 
+        if(currentIndex == sliderItems.length - 1) {
+            direction = 'right';
+            currentIndex--;
+        } else currentIndex++;
+    } else if(direction == 'right') {
+        if(currentIndex == 0) {
+            direction = 'left';
+            currentIndex++;
+        } else currentIndex--;
+    }
+}
+
 let timer = setInterval(move, 5500);
-
-
-
-sliderEllipses.forEach((item, index) => {
+ 
+sliderEllipses.forEach( item => {
+    let btnIndex = +item.getAttribute('index');
     item.addEventListener('click', function() {
         clearInterval(timer);
-        move(index);
+        currentIndex = btnIndex;
+        move();
+        timer = setInterval(move, 5500);
     });
 })
