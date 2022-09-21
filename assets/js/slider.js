9add785ed6ddb9b0454b52d9640b719c966b3a1d
TruwerM5 <ilsur2000.2016@gmail.com>
2022-09-21 23:09:53 +0300

    add changes

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,49 +1,68 @@
-'use strict';
 
-const slider__inner = document.querySelector('[data-slider]');
-const sliderItems = document.querySelectorAll('[data-slider-item]')
-const sliderEllipses = document.querySelectorAll('[data-slider-ellipse]');
+window.addEventListener('load', function(){
 
-let currentIndex = 1;
-let direction = 'left';
 
-for(let i = 0; i < sliderEllipses.length; i++) {
-    sliderEllipses[i].setAttribute('index', i);
-}
 
-function styleButtons(index) {
+    'use strict';
+
+    const slider__inner = document.querySelector('[data-slider]');
+    const sliderItems = document.querySelectorAll('[data-slider-item]')
+    const sliderEllipses = document.querySelectorAll('[data-slider-ellipse]');
+    const sliderPrevBtn = document.querySelector('[data-slide-prev]');
+    const sliderPrevNext = document.querySelector('[data-slide-next]');
+
+
+    let currentIndex = 1;
+    let direction = 'left';
+    
+
+
+
     for(let i = 0; i < sliderEllipses.length; i++) {
-        sliderEllipses[i].classList.remove('active');
-        if(i == index) {
-            sliderEllipses[i].classList.add('active');
+        sliderEllipses[i].setAttribute('index', i);
+    }
+    
+    function styleButtons(index) {
+        for(let i = 0; i < sliderEllipses.length; i++) {
+            sliderEllipses[i].classList.remove('active');
+            if(i == index) {
+                sliderEllipses[i].classList.add('active');
+            }
         }
     }
-}
-
-function move() {
-    styleButtons(currentIndex);
-    slider__inner.style.transform = `translateX(${-currentIndex * 100}%)`;
-    if(direction == 'left') { 
-        if(currentIndex == sliderItems.length - 1) {
-            direction = 'right';
-            currentIndex--;
-        } else currentIndex++;
-    } else if(direction == 'right') {
-        if(currentIndex == 0) {
-            direction = 'left';
-            currentIndex++;
-        } else currentIndex--;
+    
+    function move() {
+        styleButtons(currentIndex);
+        slider__inner.style.transform = `translateX(${-currentIndex * 100}%)`;
+        if(direction == 'left') { 
+            if(currentIndex == sliderItems.length - 1) {
+                direction = 'right';
+                currentIndex--;
+            } else currentIndex++;
+        } else if(direction == 'right') {
+            if(currentIndex == 0) {
+                direction = 'left';
+                currentIndex++;
+            } else currentIndex--;
+        }
+        
     }
-}
-
-let timer = setInterval(move, 5500);
- 
-sliderEllipses.forEach( item => {
-    let btnIndex = +item.getAttribute('index');
-    item.addEventListener('click', function() {
-        clearInterval(timer);
-        currentIndex = btnIndex;
-        move();
-        timer = setInterval(move, 5500);
+    
+    let timer = setInterval(move, 5500);
+     
+    sliderEllipses.forEach( item => {
+        let btnIndex = +item.getAttribute('index');
+        item.addEventListener('click', function() {
+            clearInterval(timer);
+            currentIndex = btnIndex;
+            
+            move();
+            console.log(currentIndex);
+            timer = setInterval(move, 5500);
+        });
     });
-})
+
+});
+
+
+
