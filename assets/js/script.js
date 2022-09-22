afe8d01c8674afda1cbe10ae885171cc7b8cd88f
TruwerM5 <ilsur2000.2016@gmail.com>
2022-09-22 22:33:30 +0300

    add changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,3 +24,21 @@ if(readMoreBtn) {
         hero.classList.toggle('enabled');
     })
 }
+
+
+/*
+CAROUSEL
+*/ 
+
+const carousel__inner = document.querySelector('[data-carousel]');
+const btnPrev = document.querySelector('[data-carousel-prev]');
+const btnNext = document.querySelector('[data-carousel-next]');
+
+
+btnPrev.addEventListener('click', function(){
+    carousel__inner.scrollLeft = 0;
+});
+
+btnNext.addEventListener('click', function(){
+    carousel__inner.scrollLeft = carousel__inner.offsetWidth;
+});
